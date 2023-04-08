1f08657140f45757636fd607d0e8588f64c1ffd6
mario.morisi <mario.morisi@openmindonline.it>
2023-04-08 11:51:58 +0200

    fix effetto elefante

--- elephantEffect.js
+++ elephantEffect.js
@@ -2,7 +2,7 @@ const ghostElephant = document.querySelector(".elephantImage_ghost");
 const firstContainer = document.querySelector(".first-container");
 
 window.onscroll = () => {
-    if (window.scrollY > firstContainer.getBoundingClientRect().bottom) {
+    if (window.scrollY > firstContainer.getBoundingClientRect().top) {
         ghostElephant.classList.remove('hide');
     } else {
         ghostElephant.classList.add('hide');
